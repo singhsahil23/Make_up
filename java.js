d069237a4cedf5d57b3af867ffbd9537fd20a3be
singhsahil23 <singhsahil20171999@gmail.com>
2025-12-13 02:22:23 +0530

    maa ka pyaar

---
+++ java.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+function addToCart(product) {
+    alert(product + " added to your cart!");
+}
